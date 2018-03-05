34dc6cc5d06557014883afd42756bf5db0fd5149
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 19:34:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   return nil if !name_hash.any?
 
   minkey, minvalue = name_hash.first
-  
+
   name_hash.each do |key, value|
     if minvalue > value
       minkey = key
